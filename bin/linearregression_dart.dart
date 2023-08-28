7457160956edef85e253ed5743e0a6a2c519e5c1
Andrii Marusyk <andrii@Andriis-MacBook-Pro.local>
2023-08-28 12:04:15 +0300

    finished the demo example with linear regression on housing dataset

--- bin/linearregression_dart.dart
+++ bin/linearregression_dart.dart
@@ -1,9 +1,32 @@
 import 'package:linearregression_dart/linearregression_dart.dart';
-import 'package:linearregression_dart/vector.dart';
+import 'package:linearregression_dart/parsers/csv_parser.dart';
+import 'package:linearregression_dart/scalers/minmax.dart';
 
-void main(List<String> arguments) {
-  var X = List.generate(100, (index) => Vector([(index + 1) / 100]));
-  var y = Vector(List.generate(100, (index) => ((index + 1) * 2) / 200));
+void main(List<String> arguments) async {
+  const path = '/Users/andrii/projects/Dart-LinearRegression/data/housing.csv';
+
+  final parser = CsvParser<double>();
+
+  var housingData = await parser.parse(path);
+
+  housingData.columns = [
+    'price',
+    'area',
+    'bedrooms',
+    'bathrooms',
+    'stories',
+    'parking'
+  ];
+
+  print(housingData);
+
+  final scaler = MinMaxScaler<double>();
+  housingData = scaler.scaleDf(housingData);
+
+  print(housingData);
+
+  var X = housingData.drop('price').toVectors();
+  var y = housingData['price'];
 
   final regressor = LinearRegression(X: X, y: y, learningRate: 0.001);
   regressor.fit(1000);
